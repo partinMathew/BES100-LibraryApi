254c27230c164cc3278409bc0dc6598f973ffd6c
Mathew Partin <partinmathew@gmail.com>
2020-01-23 15:33:00 -0500

    end of class

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,5 +1,6 @@
 ﻿using LibraryApi2.Domain;
 using LibraryApi2.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -19,13 +20,95 @@ namespace LibraryApi2.Controllers
             _context = context;
         }
 
+        /// <summary>
+        /// Gives you all the books that are currently in inventory
+        /// </summary>
+        /// <returns>Those books.</returns>
         IQueryable<Book> GetBooksInInventory()
         {
             return _context.Books.Where(b => b.InInventory);
         }
 
+        [HttpPut("/books/{id:int}/genre")]
+        public async Task<IActionResult> UpdateTheGenre(int id, [FromBody] string genre)
+        {
+            var book = await GetBooksInInventory().SingleOrDefaultAsync(b => b.Id == id);
+            if(book == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                book.Genre = genre;
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+        }
+
+        [HttpDelete("/books/{id:int}")]
+        public async Task<IActionResult> RemoveBookFromInventory(int id)
+        {
+            var book = await GetBooksInInventory().SingleOrDefaultAsync(b => b.Id == id);
+
+            if (book != null)
+            {
+                book.InInventory = false;
+                await _context.SaveChangesAsync();
+            }
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Add a book to the inventory
+        /// </summary>
+        /// <param name="bookToAdd">Information about the book you want to add</param>
+        /// <returns></returns>
+        [HttpPost("/books")]
+        [Produces("application/json")]
+        public async Task<ActionResult<GetBookDetailsResponse>> AddABook([FromBody] PostBooksRequest bookToAdd)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+            var book = new Book
+            {
+                Title = bookToAdd.Title,
+                Author = bookToAdd.Author,
+                Genre = bookToAdd.Genre,
+                NumberOfPages = bookToAdd.NumberOfPages,
+                InInventory = true
+            };
+
+            _context.Books.Add(book);
+
+            await _context.SaveChangesAsync();
+
+            var response = new GetBookDetailsResponse
+            {
+                Id = book.Id,
+                Title = book.Title,
+                Author = book.Author,
+                Genre = book.Genre,
+                NumberOfPages = book.NumberOfPages
+            };
+
+
+            return CreatedAtRoute("books#getbookbyid", new { id = response.Id }, response);
+        }
+
+        /// <summary>
+        /// Provides a list of all the books in our inventory.
+        /// </summary>
+        /// <param name="genre">If you'd like to be able to filter by genre, use this. Otherwise all books will be returned.</param>
+        /// <returns>A list of books.</returns>
+        /// <response code="200">Returns all of your books.</response>
         [HttpGet("/books")]
-        public async Task<IActionResult> GetAllBooks([FromQuery] string genre = "all")
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<GetBooksResponse>> GetAllBooks([FromQuery] string genre = "all")
         {
 
             var books = GetBooksInInventory();
@@ -53,7 +136,7 @@ namespace LibraryApi2.Controllers
             return Ok(response);
         }
 
-        [HttpGet("/books/{id:int}")]
+        [HttpGet("/books/{id:int}", Name = "books#getbookbyid")]
         public async Task<IActionResult> GetBookById(int id)
         {
             var response = await GetBooksInInventory().Where(b => b.Id == id)
--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,10 @@ using Microsoft.Extensions.Logging;
 using LibraryApi2.Services;
 using LibraryApi2.Domain;
 using Microsoft.EntityFrameworkCore;
+using LibraryApi.Services;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.IO;
 
 namespace LibraryApi2
 {
@@ -28,14 +32,37 @@ namespace LibraryApi2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(options =>
+                options.JsonSerializerOptions.Converters.Add(new StringToIntConverter())
+            );
+
             // Every time an instance of the interface is requested, a new instance of the EnrollmentIdGenerator will be created
             services.AddTransient<IGenerateEnrollmentIds, EnrollmentIdGenerator>();
 
             services.AddDbContext<LibraryDataContext>(options =>
-                options.UseSqlServer("server=.\\sqlexpress;database=library;integrated security=true")
+                options.UseSqlServer(Configuration.GetConnectionString("LibraryDatabase"))
 
             );
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Library Api",
+                    Version = "1.0",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Mat Partin",
+                        Email = "partinmathew@gmailcom"
+                    },
+                    Description = @"This is the API for our library. Make sure you pay your late fees!"
+                });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,7 +73,15 @@ namespace LibraryApi2
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Library API");
+                c.RoutePrefix = string.Empty;
+            });
+
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
